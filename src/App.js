48e208cf5d8d540b636cd077a96a6d977e0deabf
Catalin Albu <cataablu1919@gmail.com>
2023-02-14 22:44:50 +0200

    Create a basic project with a counter using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { actions } from './store';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const [value, setValue] = useState();
+  const handleIncrement = () => {
+    dispatch(actions.increment());
+  };
+  const handleDecrement = () => {
+    dispatch(actions.decrement());
+  };
+  const handleAddBy = () => {
+    dispatch(actions.addBy(value));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <input onChange={(e) => setValue(e.target.value)} />
+      <button onClick={handleAddBy}>Add by</button>
     </div>
   );
 }
